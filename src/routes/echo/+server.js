3cdf06d01fb7ffa7e63a3ebf40c636030bc7bfba
Alastair Tse <alastair@liquidx.net>
2024-11-01 20:48:32 +0900

    Make echo allow customizing where to point the readdir method

--- src/routes/echo/+server.js
+++ src/routes/echo/+server.js
@@ -1,24 +1,26 @@
-import sharp from "sharp";
-import process from "process";
-import fs from "fs";
-import { json } from "@sveltejs/kit";
+import sharp from 'sharp';
+import process from 'process';
+import fs from 'fs';
+import { json } from '@sveltejs/kit';
 
 // Forces the use of the fonts in the lambda layer.
-process.env.FONTCONFIG_PATH='/var/task/.netlify/server/fonts'
+process.env.FONTCONFIG_PATH = '/var/task/.netlify/server/fonts';
 
-const fontFamily = 'Roboto'
+const fontFamily = 'Roboto';
 
+export const GET = async ({ url }) => {
+	let localPath = url.searchParams.path;
+	if (!localPath) {
+		localPath = '/var/task/.netlify/server';
+	}
+	console.log(process.env);
+	let error = '';
+	let dirfiles = [];
+	try {
+		dirfiles = fs.readdirSync(localPath);
+	} catch (e) {
+		error = e.toString();
+	}
 
-export const GET = async (req) => {
-  console.log(process.env)
-  let error = ''
-  let dirfiles = []
-  try {
-    dirfiles = fs.readdirSync('/var/task/.netlify/server');
-  } catch (e) {
-    error = e.toString();
-  } 
-
-  return json({env: process.env, dirfiles, error});
-
-}
+	return json({ env: process.env, dirfiles, error });
+};
